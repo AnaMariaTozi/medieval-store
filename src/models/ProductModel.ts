8168af7c9e0bb4dec2405a9ae301c20892da9e6d
Ana Maria Tozi <anamariatozi@gmail.com>
2022-08-05 10:47:30 -0300

    feat: model product create and getAll

--- src/models/ProductModel.ts
+++ src/models/ProductModel.ts
@@ -7,6 +7,17 @@ export default class ProductModel {
   public constructor(connection: Pool) {
     this.connection = connection;
   }
+
+  public async create(product: Product): Promise<Product> {
+    const { name, amount } = product;
+    const result = await this.connection.execute<ResultSetHeader>(
+      'INSERT INTO products (name, amount) VALUES (?, ?)',
+      [name, amount],
+    );
+    const [dataInserted] = result;
+    const { insertId } = dataInserted;
+    return { id: insertId, ...product };
+  }
   
   public async getAll(): Promise<Product[]> {
     const result = await this.connection.execute('SELECT * FROM products');
