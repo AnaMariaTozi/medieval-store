87d4177b7583274dfc2d509ef1fe9f0f360ccb0a
Ana Maria Tozi <anamariatozi@gmail.com>
2022-08-05 17:02:23 -0300

    feat: controller users create

---
+++ src/controllers/users.controller.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+
+import UsersService from '../services/users.service';
+
+export default class UsersController {
+  constructor(private usersService = new UsersService()) { }
+
+  public create = async (req: Request, res: Response) => {
+    const user = req.body;
+    const userCreated = await this.usersService.create(user);
+    res.status(StatusCodes.CREATED).json(userCreated);
+  };
+}
