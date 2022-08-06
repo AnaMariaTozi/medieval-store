2fb8d44f34f505d6b6e16119580546b22bb15ab6
Ana Maria Tozi <anamariatozi@gmail.com>
2022-08-06 00:06:23 -0300

    feat: controller orders create and list

---
+++ src/controllers/orders.controller.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+
+import OrdersService from '../services/orders.service';
+
+export default class OrdersController {
+  constructor(private ordersService = new OrdersService()) { }
+
+  public create = async (req: Request, res: Response) => {
+    const order = req.body;
+    const orderCreated = await this.ordersService.create(order);
+    res.status(StatusCodes.CREATED).json(orderCreated);
+  };
+    
+  public getAll = async (_req: Request, res: Response) => {
+    const orders = await this.ordersService.getAll();
+    return res.status(StatusCodes.OK).json(orders);
+  };
+}
