263fa3f1148b6573e3b7aed34a16af3b431fd6c3
Ana Maria Tozi <anamariatozi@gmail.com>
2022-08-06 00:07:03 -0300

    feat: model querys create and list orders

---
+++ src/models/orders.model.ts
@@ -0,0 +1,35 @@
+import { Pool, ResultSetHeader } from 'mysql2/promise';
+import { Order } from '../interfaces/orders.interface';
+
+export default class OrderModel {
+  private connection: Pool;
+
+  public constructor(connection: Pool) {
+    this.connection = connection;
+  }
+    
+  public async create(userId: number) {
+    const result = await this.connection.execute<ResultSetHeader>(
+      'INSERT INTO Trybesmith.Orders (userId) VALUES (?)',
+      [userId],
+    );
+    const [dataInserted] = result;
+    const { insertId } = dataInserted;
+    return insertId;
+  }
+    
+  public async getAll(): Promise<Order[]> {
+    const result = await this.connection.execute(
+      `SELECT O.id, O.userId, P.id AS productsIds
+            FROM 
+            Trybesmith.Orders AS O
+            JOIN 
+            Trybesmith.Products AS P 
+            ON O.id = P.orderId
+            GROUP BY O.id
+            ORDER BY O.userId`,
+    );
+    const [rows] = result;
+    return rows as Order[];
+  }
+}
