538d458ada7f5defe0db5d2db5fcf4c629e147cb
Ana Maria Tozi <anamariatozi@gmail.com>
2022-08-07 23:51:39 -0300

    feat: add autj token user

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,14 +1,18 @@
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import JwtService from '../services/jwt.service';
 
 import UsersService from '../services/users.service';
 
 export default class UsersController {
-  constructor(private usersService = new UsersService()) { }
+  constructor(
+    private usersService = new UsersService(),
+    private jwtCreate = new JwtService(),
+  ) { }
 
   public create = async (req: Request, res: Response) => {
-    const user = req.body;
-    const userCreated = await this.usersService.create(user);
-    res.status(StatusCodes.CREATED).json(userCreated);
+    const user = await this.usersService.create(req.body);
+    const token = this.jwtCreate.createToken(user);
+    res.status(StatusCodes.CREATED).json({ token });
   };
 }
